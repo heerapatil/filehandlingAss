a571eb74828b5888e41269c23dacd4b06f971b09
heerapatil <heera.patil1306@gmail.com>
2017-06-09 15:54:06 +0530

    Create assignment11.py

--- assignment11.py
+++ assignment11.py
@@ -1,5 +1,14 @@
 #11. Write a Python program to write a list to a file.
-import simplejson
+'''import simplejson
 f = open("test.txt", "w")
 simplejson.dump([1,2,3,4], f)
 f.close()
+'''
+
+list=["one","two","three"]
+with open("text.txt", "w") as f:
+        for l in list:
+                f.write("%s\n" % l)
+
+content = open("text.txt")
+print(content.read())
