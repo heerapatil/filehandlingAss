22e17dfbb0487323762ec692d3b9da25c6c96b08
heerapatil <heera.patil1306@gmail.com>
2017-06-09 15:21:44 +0530

    Create assignment14.py

--- assignment14.py
+++ assignment14.py
@@ -1,6 +1,6 @@
 #14. Write a Python program to read a random line from a file. 
 
 import random
- lines = open("text.txt").read().splitlines()
- print lines
+lines = open("text.txt").read().splitlines()
+print lines
 
