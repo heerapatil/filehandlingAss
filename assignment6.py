3c40f903b08347fac217e7074d1acf85a9b9c031
heerapatil <heera.patil1306@gmail.com>
2017-06-09 11:26:03 +0530

    Create assignment6.py

--- assignment6.py
+++ assignment6.py
@@ -1,5 +1,13 @@
+#6. Write a Python program to read a file line by line store it into a variable. 
+
 def file(fname):
         with open (fname, "r") as f:
                 data=f.readlines()
                 print(data)
 file('text.txt')
+
+
+'''output:
+['hello people\n', 'hi\n', 'how\n', 'are\n', 'you\n']
+'''
+
