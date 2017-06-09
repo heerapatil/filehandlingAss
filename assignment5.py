937df2224a1baa0cd5ecfd14bf21821b43f8d485
heerapatil <heera.patil1306@gmail.com>
2017-06-09 10:26:59 +0530

    Create assignment5.py

---
+++ assignment5.py
@@ -0,0 +1,7 @@
+#5. Write a Python program to read a file line by line and store it into a list. 
+
+def read_file("text.txt"):  
+ with open("text.txt") as f:        
+ print(f.readlines())  
+ 
+  
