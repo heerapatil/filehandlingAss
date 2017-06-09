2ee323b0d58929cb7b2be332f02f2dd4c81db7e7
heerapatil <heera.patil1306@gmail.com>
2017-06-09 14:37:29 +0530

    Create assignment16.py

--- assignment16.py
+++ assignment16.py
@@ -1,5 +1,11 @@
-#16. Write a Python program to remove newline characters from a file.
+'''#16. Write a Python program to remove newline characters from a file.
 f=open("text.txt","r").readlines()
 for s in f:
     s.rstrip("\n")
     print text.txt
+'''
+def remove_newlines(fname):
+    flist = open(fname).readlines()
+    return [s.rstrip('\n') for s in flist]
+
+print(remove_newlines("text.txt"))
