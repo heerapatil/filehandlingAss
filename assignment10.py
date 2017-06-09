2789983617c011f9e32c232c0e5ad1e311fea9f3
heerapatil <heera.patil1306@gmail.com>
2017-06-09 15:01:16 +0530

    Create assignment10.py

--- assignment10.py
+++ assignment10.py
@@ -1,10 +1,12 @@
 #10. Write a Python program to get the file size of a plain file. 
 import os
-def getSize(file):
+'''def getSize(file):
     st = os.stat(file)
     return st.st_size
 print getSize("text.txt")
-
+'''
+st=os.st_size("text.txt")
+print st
 
 '''output:
 70
