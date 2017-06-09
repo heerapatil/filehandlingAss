8012e7095e1c44e70bd2944e097d605a28a56440
heerapatil <heera.patil1306@gmail.com>
2017-06-09 15:34:00 +0530

    Create assignment13.py

--- assignment13.py
+++ assignment13.py
@@ -3,3 +3,20 @@
 with open("text.txt") as f1, open("test.txt") as f2:
     for l1, l2 in zip(f1, f2):
        print(l1+l2)
+'''output:
+hello people
+hello people
+hi
+hi
+how
+how
+are 
+are
+you
+you
+fsdfgsdhf
+fsdfgsdhf
+dfsfksdf
+dfsfksdf
+dfsdfsdf
+dfsdfsdf'''
