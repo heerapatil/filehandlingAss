48c5f7abf91527e2bd811ba3953f2f5413507efc
heerapatil <heera.patil1306@gmail.com>
2017-06-09 17:06:30 +0530

    Create assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -1,7 +1,7 @@
 #3. Write a Python program to append text to a file and display the text.
 
 from itertools import islice
- with open("text.txt", "w") as f:
+with open("text.txt", "w") as f:
         f.write("heera")
         f.write("patil")
- print text.txt
+print text.txt
