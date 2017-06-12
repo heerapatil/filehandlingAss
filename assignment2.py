90d184ac2347a37db6dafb24edef89af863fb6d7
heerapatil <heera.patil1306@gmail.com>
2017-06-12 11:58:34 +0530

    Create assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,7 +1,7 @@
 #2. Write a Python program to read first n lines of a file and print these lines.
 def file(fname, nlines):  
-        from itertools import islice  
-        with open(fname) as f:  
+      from itertools import islice  
+      with open(fname) as f:  
                 for line in islice(f, nlines):  
                         print(line)  
 file('text.txt',2)  
