f3e0d9ecd5b253b9985d1fc038547b767ad33522
heerapatil <heera.patil1306@gmail.com>
2017-06-12 12:17:19 +0530

    Create assignment4.py

---
+++ assignment4.py
@@ -0,0 +1,14 @@
+# Question 4: Write a Python program to read last n lines of a file and display the same
+
+def file(fname, nlines):  
+        from itertools import islice  
+        with open(fname) as f:  
+                for line in islice(f, nlines):  
+                        print(line)  
+file("test.txt",-2)  
+
+
+'''
+output:
+
+'''
