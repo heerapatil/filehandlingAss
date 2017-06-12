d583157088fe2b3d6f87686bef30d642d8ae116f
heerapatil <heera.patil1306@gmail.com>
2017-06-12 10:39:07 +0530

    Create assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,5 +1,9 @@
+#1. Write a Python program to read an entire text file and print its contents.
 f=open("file1.txt","w+")
 f.write("hello people, how are you all")
 f.seek(0,0)
 print(f.read())
 f.close()
+'''output:
+hello people, how are you all
+'''
